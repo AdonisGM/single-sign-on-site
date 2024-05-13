22879ff286a6df3d9c367e30ea3b4e8b2b811cc4
Adonis Willer <nmtung.study@gmail.com>
2024-05-13 20:22:25 +0700

    Remove cookie confirm

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
-import {motion, useAnimate} from "framer-motion";
 import './App.css'
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-import CookieImage from "./assets/cookie-svgrepo-com.svg";
-import {Button, Card, Image} from "@nextui-org/react";
-import {Fragment, useEffect} from "react";
 import {Toaster} from "react-hot-toast";
 
 import Signup from "./components/signup/Signup.jsx";
@@ -57,14 +53,6 @@ const router = createBrowserRouter([
 ]);
 
 function App() {
-  const [scope, animate] = useAnimate();
-
-  useEffect(() => {
-    if (!localStorage.getItem('isAllowCookie')) {
-      animate(scope.current, {opacity: 1, y: 0, scale: 1});
-    }
-  }, [animate, scope]);
-
   return (
     <div>
       <RouterProvider router={router}/>
@@ -74,31 +62,6 @@ function App() {
         }}
         position="top-right"
       />
-      <motion.div
-        className={'flex justify-center items-center fixed bottom-0 w-full'}
-        initial={{opacity: 0, y: 100, scale: 0}}
-        ref={scope}
-      >
-        <Card
-          className={'flex flex-row items-center gap-2 p-2 bg-default-700 text-white w-fit m-2'}
-        >
-          <Image src={CookieImage} className={'w-6 h-6 min-h-6 min-w-6'}/>
-          <Fragment>
-            <p className={'text-xs text-center text-default-300 w-full'}>
-              We need cookies to run this app. Sorry for the inconvenience!
-            </p>
-            <Button
-              size="sm"
-              onPress={() => {
-                localStorage.setItem('isAllowCookie', true);
-                animate(scope.current, {opacity: 0, y: 100, scale: 0});
-              }}
-            >
-              Accept
-            </Button>
-          </Fragment>
-        </Card>
-      </motion.div>
     </div>
   )
 }
